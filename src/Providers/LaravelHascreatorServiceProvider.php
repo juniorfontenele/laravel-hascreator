2de60e10fc9dfc835a8cf16fdc3b6c2ca97cf33a
Junior Fontenele <lcbfjr@gmail.com>
2024-07-16 14:43:51 -0300

    Initial commit

---
+++ src/Providers/LaravelHascreatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JuniorFontenele\LaravelHascreator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHascreatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace JuniorFontenele\LaravelHascreator\Tests;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
